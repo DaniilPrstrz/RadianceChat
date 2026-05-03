784dc25ab0a1ef7e0f0e85ce87d1151e48bd388e
YoungSusanoo <ksovfox@mail.ru>
2026-05-03 17:07:12 +0300

    Fix models bugs

---
+++ models/user.go
@@ -0,0 +1,22 @@
+package models
+
+import "time"
+
+type User struct {
+	ID        string    `json:"id"`
+	Username  string    `json:"username"`
+	Email     string    `json:"email"`
+	Password  string    `json:"-"`
+	CreatedAt time.Time `json:"created_at"`
+	Status    string    `json:"status"` // online, offline
+}
+
+type AuthRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type AuthResponse struct {
+	Token string `json:"token"`
+	User  *User  `json:"user"`
+}
